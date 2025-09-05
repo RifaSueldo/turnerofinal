ec4b8fd9b85fa525450bd0a4b4de11cdd852f94c
Turnox <weebargentina@gmail.com>
2025-09-05 08:01:28 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "turnox-cache-v2"; // Cambia este número cuando quieras forzar actualización
+const CACHE_NAME = "turnox-cache-v3"; // Cambia este número cuando quieras forzar actualización
 const urlsToCache = [
   "/",
   "/index.html",
