233e3890ba3fc2a5b7005b2601b9d2f539d9f1cb
Turnox <weebargentina@gmail.com>
2025-09-04 22:29:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
+// ------------------ SERVICE WORKER ------------------
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js")
+      .then(reg => {
+        console.log("SW registrado:", reg);
+
+        // Detectar nueva versión
+        reg.addEventListener("updatefound", () => {
+          const newSW = reg.installing;
+          newSW.addEventListener("statechange", () => {
+            if (newSW.state === "installed" && navigator.serviceWorker.controller) {
+              // Nueva versión lista: fuerza recarga
+              console.log("Nueva versión disponible");
+              newSW.postMessage({ type: "SKIP_WAITING" });
+              window.location.reload();
+            }
+          });
+        });
+      })
+      .catch(err => console.error("Error SW:", err));
+  });
+}
+
+
+
 function hideLoading() {
   const loading = document.getElementById("loadingScreen");
   if (loading) {
